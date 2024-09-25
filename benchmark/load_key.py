776125f8d7d80c033f6419a21b1a8a0570045984
lty <lty576@outlook.com>
2024-09-24 22:22:06 -0700

    2024.9.25,13:30,six

--- benchmark/load_key.py
+++
@@ -1,8 +0,0 @@
-from paramiko import SSHClient, RSAKey, AutoAddPolicy
-
-client = SSHClient()
-client.set_missing_host_key_policy(AutoAddPolicy())
-private_key = RSAKey(filename='~/.ssh/aws')
-print("Using private key:", private_key)
-client.connect('hostname', username='your_user', pkey=private_key)
-
